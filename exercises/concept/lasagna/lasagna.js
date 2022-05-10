590da0a2ed39631a4b4ea9d86f7fd77f845df7f5
eyescream <stilghar.dev@gmail.com>
2022-05-10 23:55:09 +0800

    done lasagna exercise

--- exercises/concept/lasagna/lasagna.js
+++ exercises/concept/lasagna/lasagna.js
@@ -34,6 +34,7 @@
  * The number of minutes it takes to prepare a single layer.
  */
 const PREPARATION_MINUTES_PER_LAYER = 2;
+export const EXPECTED_MINUTES_IN_OVEN = 40;
 
 /**
  * Determines the number of minutes the lasagna still needs to remain in the
@@ -43,7 +44,7 @@ const PREPARATION_MINUTES_PER_LAYER = 2;
  * @returns {number} the number of minutes remaining
  */
 export function remainingMinutesInOven(actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
 }
 
 /**
@@ -53,7 +54,7 @@ export function remainingMinutesInOven(actualMinutesInOven) {
  * @returns {number} the total preparation time
  */
 export function preparationTimeInMinutes(numberOfLayers) {
-  throw new Error('Remove this line and implement the function');
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER;
 }
 
 /**
@@ -65,5 +66,6 @@ export function preparationTimeInMinutes(numberOfLayers) {
  * @returns {number} the total working time
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  // throw new Error('Remove this line and implement the function');
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
 }
