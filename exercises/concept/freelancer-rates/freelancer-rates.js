8217b546438599c6bfcfa05e7bd1263655b8d5a4
eyescream <stilghar.dev@gmail.com>
2022-05-13 03:59:53 +0800

    simplify

--- exercises/concept/freelancer-rates/freelancer-rates.js
+++ exercises/concept/freelancer-rates/freelancer-rates.js
@@ -49,9 +49,9 @@ export function daysInBudget(budget, ratePerHour) {
  * @returns {number} the rounded up discounted rate
  */
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-  let totalPerMonth = dayRate(ratePerHour) * 22 ;
-  let totalMonths = Math.floor(numDays / 22);
-  let grossMonths = totalPerMonth * totalMonths;
-  return Math.ceil((grossMonths - (grossMonths * discount)) 
+  const totalPerMonth = dayRate(ratePerHour) * 22 ;
+  const totalMonths = Math.floor(numDays / 22);
+  const totalGross = totalPerMonth * totalMonths;
+  return Math.ceil((totalGross - (totalGross * discount)) 
   + ((numDays % 22) * dayRate(ratePerHour)));
 }
